3c601f6de618c6664f9de6e0f49e8ffeaed55cb8
nitinLogic <nitin.yadav21193@gmail.com>
2025-12-24 09:59:39 +0530

    Add new file & folder

---
+++ JS/GoodString.js
@@ -0,0 +1,7 @@
+let str = "abc";
+if(str.length > 3 && str[0] === 'a'){
+    console.log("good string");
+}
+else{
+    console.log("not good string");
+}
---
+++ JS/TrafficLight.js
@@ -0,0 +1,11 @@
+let light = "yellow";
+
+if(light == "green"){
+    console.log(`Go. Light color is ${light}`);
+}
+else if(light == "red"){
+    console.log(`Stop. Light color is ${light}`);
+}
+else{
+    console.log(`Slow Down. Light color is ${light}`);
+}
