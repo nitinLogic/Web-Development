e795f6aba795051909ba7069a2d7efe6d035471e
nitinLogic <nitin.yadav21193@gmail.com>
2025-12-29 12:17:34 +0530

    Add new file

---
+++ JS/Poem.js
@@ -0,0 +1,8 @@
+function printPoem(){
+    console.log("Twinkle Twinkle, little star");
+    console.log("How I wonder what you are");
+    console.log("Up above the world so high");
+    console.log("Like a diamond in the sky");
+}
+
+printPoem();
